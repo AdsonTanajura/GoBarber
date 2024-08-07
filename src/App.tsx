de0177a180b2f89d20af34be7c50a8c4006cec2b
Adson <adson.tanajura@gmail.com>
2024-08-07 16:10:43 -0300

    adiciona o HTML da pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
+
 import GlobalStyles from './styles/global';
+import SingnIn from './pages/Signin';
 
 
 const App: React.FC = () => (
   <>
-    <h1>Hello Word</h1>
-    <GlobalStyles/>
+    <SingnIn /> 
+    <GlobalStyles />
   </>
 
 );
---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import logoImg from '../../assets/Logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SingnIn: React.FC = () =>(
+ <Container>
+    <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+            <h1>Faca seu logon</h1>
+            <input placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <button type="submit">Entrar</button>
+            <a href="fotgot">Esqueci minha senha</a>
+        </form>
+
+        <a href="">Criar conta</a>
+    </Content>
+    <Background />
+ </Container>
+);
+
+export default SingnIn
